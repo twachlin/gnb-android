77cd5d3af758aeaae55c2a3dfb396a2d3d670a61
twachlin <tomaswachlin@gmail.com>
2023-01-31 09:48:52 -0300

    Add Ui states

---
+++ app/src/main/java/com/gnb_android/commons/ui/viewmodel/states/UiStates.kt
@@ -0,0 +1,25 @@
+package com.gnb_android.commons.ui.viewmodel.states
+
+/**
+ * Possible UI states
+ */
+sealed class UiState<out Data>
+
+/**
+ * Loading
+ */
+class Loading(val isFinished: Boolean = false) : UiState<Nothing>()
+
+/**
+ * Success State
+ */
+sealed class SuccessUiState<out Data> : UiState<Data>()
+
+object Empty : SuccessUiState<Nothing>()
+
+class Success<out Data>(val body: Data) : SuccessUiState<Data>()
+
+/**
+ * Business Error State
+ */
+object BusinessError : UiState<Nothing>()
---
+++ app/src/main/java/com/gnb_android/commons/ui/viewmodel/states/extensions/DataStateExtensions.kt
@@ -0,0 +1,24 @@
+package com.gnb_android.commons.ui.viewmodel.states.extensions
+
+import com.gnb_android.commons.data.repository.states.BusinessErrorDataState
+import com.gnb_android.commons.data.repository.states.DataState
+import com.gnb_android.commons.data.repository.states.EmptyDataState
+import com.gnb_android.commons.data.repository.states.SuccessDataState
+import com.gnb_android.commons.ui.viewmodel.states.BusinessError
+import com.gnb_android.commons.ui.viewmodel.states.Empty
+import com.gnb_android.commons.ui.viewmodel.states.Success
+import com.gnb_android.commons.ui.viewmodel.states.UiState
+
+fun <Data> DataState<Data>.mapToUiState(): UiState<Data> {
+    return when (this) {
+        is EmptyDataState -> {
+            Empty
+        }
+        is SuccessDataState -> {
+            Success(this.body)
+        }
+        is BusinessErrorDataState -> {
+            BusinessError
+        }
+    }
+}
