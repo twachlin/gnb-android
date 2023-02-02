3cbd41bd6d83b00c1275d2e32ce14f21a0638229
twachlin <tomaswachlin@gmail.com>
2023-02-02 11:15:54 -0300

    Observe Transactions State in Home Screen

--- app/src/main/java/com/gnb_android/home/ui/view/HomeScreen.kt
+++ app/src/main/java/com/gnb_android/home/ui/view/HomeScreen.kt
@@ -1,20 +1,42 @@
 package com.gnb_android.home.ui.view
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.gnb_android.R
+import com.gnb_android.commons.data.datasource.currency.CurrencyRemoteDataSource
+import com.gnb_android.commons.data.repository.currency.CurrencyRepository
 import com.gnb_android.commons.ui.view.components.header.GnbHeader
+import com.gnb_android.commons.ui.viewmodel.states.BusinessError
+import com.gnb_android.commons.ui.viewmodel.states.Empty
+import com.gnb_android.commons.ui.viewmodel.states.Loading
+import com.gnb_android.commons.ui.viewmodel.states.Success
+import com.gnb_android.home.data.datasource.transactions.TransactionsRemoteDataSource
+import com.gnb_android.home.data.repository.transactions.TransactionsRepository
+import com.gnb_android.home.data.repository.transactions.model.TransactionsBySku
+import com.gnb_android.home.ui.view.components.TransactionTitleItem
+import com.gnb_android.home.ui.viewmodel.HomeViewModel
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+    viewModel: HomeViewModel
+) {
+
+    val transactionsState = viewModel.transactionsObservable.observeAsState()
+
     Scaffold(
         topBar = {
             GnbHeader(text = R.string.home_header_text)
@@ -26,16 +48,53 @@ fun HomeScreen() {
                     .padding(horizontal = 16.dp)
             ) {
                 Text(text = stringResource(id = R.string.home_screen_content_subtitle))
-                LazyColumn {
-                    // Todo: Add list of selectable items code
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    when (val state = transactionsState.value) {
+                        is Loading -> {
+                            CircularProgressIndicator(modifier = Modifier.padding(32.dp))
+                        }
+                        Empty -> {
+                            Text(text = "Empty content")
+                        }
+                        is Success -> {
+                            val transactionsBySku: List<TransactionsBySku> = state.body
+                            LazyColumn {
+                                itemsIndexed(transactionsBySku) { index, item ->
+                                    TransactionTitleItem(
+                                        text = item.sku,
+                                        onClick = { /*TODO*/ },
+                                        isLastItem = (index == transactionsBySku.size - 1)
+                                    )
+                                }
+                            }
+                        }
+                        BusinessError -> {
+                            Text(text = "Business error content")
+                        }
+                        null -> {
+                            // Do nothing
+                        }
+                    }
                 }
             }
         }
     )
+
+    LaunchedEffect(key1 = Unit) {
+        viewModel.getTransactions()
+    }
 }
 
 @Preview
 @Composable
 fun HomeScreenPreview() {
-    HomeScreen()
+    HomeScreen(
+        HomeViewModel(
+            CurrencyRepository(CurrencyRemoteDataSource()),
+            TransactionsRepository(TransactionsRemoteDataSource())
+        )
+    )
 }
