dad69b02690efa7b5c9f1be1cb3bda8e265e70b7
twachlin <tomaswachlin@gmail.com>
2023-02-02 09:41:03 -0300

    Update transactions data management for future use

--- app/src/main/java/com/gnb_android/home/data/repository/transactions/extensions/TransactionApiModelExtensions.kt
+++ app/src/main/java/com/gnb_android/home/data/repository/transactions/extensions/TransactionApiModelExtensions.kt
@@ -2,10 +2,10 @@ package com.gnb_android.home.data.repository.transactions.extensions
 
 import com.gnb_android.commons.data.repository.currency.extensions.convertToCurrencyType
 import com.gnb_android.home.data.datasource.transactions.model.TransactionApiModel
-import com.gnb_android.home.data.repository.transactions.model.Transaction
+import com.gnb_android.home.data.repository.transactions.model.TransactionDetail
 
-fun TransactionApiModel.convertToTransaction(): Transaction =
-    Transaction(
+fun TransactionApiModel.convertToTransaction(): TransactionDetail =
+    TransactionDetail(
         sku = sku,
         amount = amount,
         currency = currency.convertToCurrencyType()
