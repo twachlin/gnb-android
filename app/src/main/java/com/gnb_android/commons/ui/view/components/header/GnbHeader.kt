d5f0ec1f73143069a2c1729e18c7b10b0ccc03b4
twachlin <tomaswachlin@gmail.com>
2023-02-06 02:43:38 -0300

    Add Ui improvements

--- app/src/main/java/com/gnb_android/commons/ui/view/components/header/GnbHeader.kt
+++ app/src/main/java/com/gnb_android/commons/ui/view/components/header/GnbHeader.kt
@@ -10,8 +10,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.gnb_android.R
 
 
@@ -27,7 +29,9 @@ fun GnbHeader(
     ) {
         Text(
             modifier = Modifier.padding(horizontal = 16.dp),
-            text = stringResource(id = text)
+            text = stringResource(id = text),
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
         )
     }
 }
